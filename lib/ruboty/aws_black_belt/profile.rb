5249730a2bb341fd624ff9b8cb29d663eb88a653
koshigoe <koshigoeb@gmail.com>
2017-02-11 22:22:01 +0900

    Add #attributes to return profile items.

--- lib/ruboty/aws_black_belt/profile.rb
+++ lib/ruboty/aws_black_belt/profile.rb
@@ -1,10 +1,8 @@
 module Ruboty
   module AwsBlackBelt
     class Profile
-      attr_accessor :email, :first_name, :last_name, :country,
-                    :zipcode, :phone, :company, :post, :business,
-                    :job, :usage, :schedule, :contact,
-                    :username
+      PROFILE_KEYS = %i(email first_name last_name country zipcode phone company post business job usage schedule contact)
+      attr_accessor :username, *PROFILE_KEYS
 
       def self.t(key)
         I18n.t(key, scope: 'ruboty.aws_black_belt.profile', default: key.to_s)
@@ -12,6 +10,10 @@ module Ruboty
 
       def initialize(username)
         @username = username
+      end
+
+      def attributes
+        PROFILE_KEYS.each_with_object({}) { |k, res| res[k] = self.send(k) }
       end
     end
   end
--- spec/ruboty/aws_black_belt/profile_spec.rb
+++ spec/ruboty/aws_black_belt/profile_spec.rb
@@ -14,4 +14,45 @@ RSpec.describe Ruboty::AwsBlackBelt::Profile do
       it { is_expected.to eq 'not_exists' }
     end
   end
+
+  describe '#attributes' do
+    subject { profile.attributes }
+
+    let(:profile) do
+      Ruboty::AwsBlackBelt::Profile.new('whoami').tap do |x|
+        x.email = 'test@example.com'
+        x.first_name = '名'
+        x.last_name = '姓'
+        x.country = '日本'
+        x.zipcode = '123-4567'
+        x.phone = '090-1234-5678'
+        x.company = '株式会社テスト'
+        x.post = '社長'
+        x.business = 'ソフトウェア & インターネット'
+        x.job = '開発者/エンジニア'
+        x.usage = 'AWSでサービスを複数本番稼動させている'
+        x.schedule = ' 未定'
+        x.contact = 'false'
+      end
+    end
+
+    it 'return profile items' do
+      expected = {
+        email: 'test@example.com',
+        first_name: '名',
+        last_name: '姓',
+        country: '日本',
+        zipcode: '123-4567',
+        phone: '090-1234-5678',
+        company: '株式会社テスト',
+        post: '社長',
+        business: 'ソフトウェア & インターネット',
+        job: '開発者/エンジニア',
+        usage: 'AWSでサービスを複数本番稼動させている',
+        schedule: ' 未定',
+        contact: 'false',
+      }
+      is_expected.to eq expected
+    end
+  end
 end
