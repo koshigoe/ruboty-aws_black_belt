f9234c7019997ba371113e2a8e09884aad84e571
koshigoe <koshigoeb@gmail.com>
2017-02-11 20:27:06 +0900

    Use I18n

--- lib/ruboty/aws_black_belt.rb
+++ lib/ruboty/aws_black_belt.rb
@@ -1,6 +1,9 @@
 require 'ruboty'
 require 'ruboty/aws_black_belt/version'
 require 'ruboty/handlers/aws_black_belt'
+require 'i18n'
+
+I18n.load_path += Dir[File.expand_path('../../../locale/*.yml', __FILE__)]
 
 module Ruboty
   module AwsBlackBelt
